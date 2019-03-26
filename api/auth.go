243bc52470fbf175a4bf12cff8543ae8c3302016
Aliou Amar <2310549a@student.gla.ac.uk>
2019-03-26 15:52:28 +0000

    bruh

--- api/auth.go
+++ api/auth.go
@@ -47,35 +47,6 @@ func serverManager(srv *http.Server, keepAlive chan bool) {
 	}
 }
 
-func GetCode(id string) (c string) {
-	m := GetAuthorizationURL(id)
-	fmt.Printf("\nNavigate to the following URL to Authorize Spotistats:\n%s\n", m)
-	keepAlive := make(chan bool)
-	server := &http.Server{Addr: ":8000"}
-
-	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
-		code := r.URL.Query().Get("code")
-		keepAlive <- true
-		fmt.Fprintf(w, "<h1>Almost done!</h1><p>Spotistats has been approved, just copy the following code back to the CLI:\n <span style=\"color: #FF0000\">%s</span></p>", code)
-	})
-
-	go serverManager(server, keepAlive)
-	server.ListenAndServe()
-
-	fmt.Print("\nEnter Code: ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	c = strings.TrimSpace(scanner.Text())
-
-	return c
-}
-
-func Authenticate(id, secret string) {
-	code := GetCode(id)
-	token = CodeAuthorize(id, secret, code)
-	fmt.Println("\nSuccessful Authentication")
-}
-
 func GetAuthorizationURL(id string) string {
 	v := url.Values{}
 	v.Set("client_id", id)
@@ -109,4 +80,33 @@ func CodeAuthorize(id, secret, code string) Token {
 	t.ClientSecret = secret
 
 	return t
+}
+
+func GetCode(id string) (c string) {
+	m := GetAuthorizationURL(id)
+	fmt.Printf("\nNavigate to the following URL to Authorize Spotistats:\n%s\n", m)
+	keepAlive := make(chan bool)
+	server := &http.Server{Addr: ":8000"}
+
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+		keepAlive <- true
+		fmt.Fprintf(w, "<h1>Almost done!</h1><p>Spotistats has been approved, just copy the following code back to the CLI:\n <span style=\"color: #FF0000\">%s</span></p>", code)
+	})
+
+	go serverManager(server, keepAlive)
+	server.ListenAndServe()
+
+	fmt.Print("\nEnter Code: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	c = strings.TrimSpace(scanner.Text())
+
+	return c
+}
+
+func Authenticate(id, secret string) {
+	code := GetCode(id)
+	token = CodeAuthorize(id, secret, code)
+	fmt.Println("\nSuccessful Authentication")
 }
